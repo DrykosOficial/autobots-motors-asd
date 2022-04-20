bea6c7026f6ab595a2accc141586109af4eedc2b
DrykosOficial <be.filippi@duocuc.cl>
2022-04-20 01:07:14 -0400

    AutoBots Motor

--- javascript/validaciones.js
+++ javascript/validaciones.js
@@ -1,96 +1,142 @@
 /*FORMULARIO DE REGISTRO CON JS*/
+if (document.body.classList.contains('inisiosecion')) {
+    var usuario = document.getElementById('user1');
+    var contrasenna = document.getElementById('pass1');
+    var contrasenna2 = document.getElementById('pass2');
+    var correo = document.getElementById('correo');
 
-var usuario = document.getElementById('user1');
-var contrasenna = document.getElementById('pass1');
-var contrasenna2 = document.getElementById('pass2');
-var correo = document.getElementById('correo');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
 
-form.addEventListener('click', e => {
-    e.preventDefault();
+        JsRegistrar();
+    });
 
-    JsRegistrar();
-});
+    function JsRegistrar() {
 
-function JsRegistrar() {
+        var usuarioValue = usuario.value.trim();
+        var correoValue = correo.value.trim();
+        var contrasennaValue = contrasenna.value.trim();
+        var contrasenna2Value = contrasenna2.value.trim();
 
-    var usuarioValue = usuario.value;
-    var correoValue = correo.value;
-    var contrasennaValue = pass.value;
-    var contrasenna2Value = pass2.value;
+        if (usuarioValue === '') {
+            setErrorFor(usuario, 'No puede dejar el usuario en blanco.')
 
-    if (usuarioValue == '') {
-        setErrorFor(usuario, 'No puede dejar el usuario en blanco.')
+        } else {
+            setSuccessFor(usuario);
+        }
+        if (correoValue === '') {
+            setErrorFor(correo, 'No puede dejar el correo electrónico en blanco');
+        } else if (!isEmail(correoValue)) {
+            setErrorFor(correo, 'No ingreso un correo electrónico válido');
+        } else {
+            setSuccessFor(correo);
+        }
 
-    } else {
-        setSuccessFor(usuario);
+        if (contrasennaValue === '') {
+            setErrorFor(contrasenna, 'No debe dejar en blanco la contrasena');
+        } else {
+            setSuccessFor(contrasenna);
+        }
+
+        if (contrasenna2Value === '') {
+            setErrorFor(contrasenna2, 'No debe dejar en blanco la contrasena');
+        } else if (contrasennaValue !== contrasenna2Value) {
+            setErrorFor(contrasenna2, 'Las contrasenas no coinciden');
+        } else {
+            setSuccessFor(contrasenna2);
+        }
+
+
     }
-    if (correoValue === '') {
-        setErrorFor(correo, 'No puede dejar el email en blanco');
-    } else if (!isEmail(correoValue)) {
-        setErrorFor(correo, 'No ingreso un email válido');
-    } else {
-        setSuccessFor(correo);
-    }
 
-    if (contrasennaValue === '') {
-        setErrorFor(contrasenna, 'No debe dejar en blanco la contrasena');
-    } else {
-        setSuccessFor(contrasenna);
-    }
-
-    if (contrasenna2Value === '') {
-        setErrorFor(contrasenna2, 'No debe dejar en blanco la contrasena');
-    } else if (contrasennaValue !== contrasenna2Value) {
-        setErrorFor(contrasenna2, 'Las contrasenas no coinciden');
-    } else {
-        setSuccessFor(contrasenna2);
+    function setSuccessFor(input) {
+        var formControl = input.parentElement;
+        formControl.className = 'formu-control success';
     }
 
 
+    function setErrorFor(input, message) {
+        var formControl = input.parentElement;
+        var small = formControl.querySelector('small');
+        formControl.className = 'formu-control error';
+        small.innerText = message;
+    }
+
+    function isEmail(correo) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(correo);
+    }
 }
 
-function setSuccessFor(input) {
-    var formControl = input.parentElement;
-    formControl.className = 'formu-control success';
+
+
+/*FORMULARIO DE CONTACTO CON JS 1 */
+if (document.body.classList.contains('formulariocontacto')) {
+    var usuario = document.getElementById('user1');
+    var telefono = document.getElementById('telefono');
+    var correo = document.getElementById('correo');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        JsContacto();
+    });
+
+    function JsContacto() {
+        var usuarioValue = usuario.value.trim();
+        var telefonoValue = telefono.value.trim();
+        var correoValue = correo.value.trim();
+        var mensajeValue = mensaje.value.trim();
+
+
+        if (usuarioValue === '') {
+            setErrorFor(usuario, 'No puede dejar el Nombre en blanco.')
+
+        } else {
+            setSuccessFor(usuario);
+        }
+
+        if (telefonoValue === '') {
+            setErrorFor(telefono, 'No puede dejar el telefono en blanco.')
+
+        } else {
+            setSuccessFor(telefono);
+        }
+
+        if (correoValue === '') {
+            setErrorFor(correo, 'No puede dejar el correo  en blanco');
+        } else if (!isEmail(correoValue)) {
+            setErrorFor(correo, 'No ingreso un correo  válido');
+        } else {
+            setSuccessFor(correo);
+        }
+
+        if (mensajeValue === '') {
+            setErrorFor(mensaje, 'No puede dejar el mensaje en blanco.')
+
+        } else {
+            setSuccessFor(mensaje);
+        }
+    }
+
+    function setSuccessFor(input) {
+        var formControl = input.parentElement;
+        formControl.className = 'form-groupoo success';
+    }
+
+
+    function setErrorFor(input, message) {
+        var formControl = input.parentElement;
+        var small = formControl.querySelector('small');
+        formControl.className = 'form-groupoo error';
+        small.innerText = message;
+    }
+
+    function isEmail(correo) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(correo);
+    }
 }
 
 
-function setErrorFor(input, message) {
-    var formControl = input.parentElement;
-    var small = formControl.querySelector('small');
-    formControl.className = 'formu-control error';
-    small.innerText = message;
-}
-
-function isEmail(correo) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(correo);
-}
-
-/*FORMULARIO DE CONTACTO CON JS */
-function JSENVIAR() {
-    var nombre = document.getElementById("name").value
-    var telefono = document.getElementById("telefono").value
-    var email = document.getElementById("email").value
-    var text = document.getElementById("mensaje").value
-
-
-
-    if (nombre.length == 0) {
-
-        document.getElementById("errorName").innerHTML = "El nombre no debe ser vacío";
-    }
-    if (telefono.length == 0) {
-        document.getElementById("errorTelefono").innerHTML = "El telefono no debe ser vacío";
-    }
-    if (email.length == 0) {
-        document.getElementById("errorEmail").innerHTML = "El email no debe ser vacío";
-    }
-    if (text.length == 0) {
-        document.getElementById("errorText").innerHTML = "Mensaje no debe ser vacío";
-    }
-
-    document.getElementById("resultado").innerHTML = total;
-}
 
 
 /*FORMULARIO INICIO SESION CON JQ */
