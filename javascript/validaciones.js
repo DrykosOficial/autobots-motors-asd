c21e62195714272827eaf033beef7f8ce5a0154a
DrykosOficial <be.filippi@duocuc.cl>
2022-04-19 21:30:41 -0400

    AutoBots Motors

--- javascript/validaciones.js
+++ javascript/validaciones.js
@@ -2,9 +2,16 @@
 (function() {
     'use strict'
 
+    var usuario = document.getElementById('user1');
+    var contrasenna = document.getElementById('pass1');
+    var contrasenna2 = document.getElementById('pass2');
+    var correo = document.getElementById('correo');
 
-    var forms = document.querySelectorAll('.needs-validation')
+    form.addEventListener('click', e => {
+        e.preventDefault();
 
+        JsRegistrar();
+    });
 
     Array.prototype.slice.call(forms)
         .forEach(function(form) {
